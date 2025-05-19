8b9e881721308184355ac5e1b739259832d656b8
amy <amy@thinkingsoftware.tw>
2025-05-19 17:39:11 +0800

    modify style: - title icon - edit style

--- src/components/EditTransactionForm.jsx
+++ src/components/EditTransactionForm.jsx
@@ -20,25 +20,27 @@ export const EditTransactionForm = ({
 
     return (
         <form onSubmit={handleEditSubmit} className="">
-            <input type="text" placeholder="Description" required 
-                value={description} 
-                onChange={(e) => {setDescriptionEdit(e.target.value)}}
-            />
-            <input type="number" placeholder="Amount" required 
-                value={transactionAmount}
-                onChange={(e) => {setTransactionAmount(e.target.value)}}
-            />
-            <input type="radio" id="expense-edit" value="expense"
-                checked = {transactionType === "expense"}
-                onChange={(e) => {settransactionType(e.target.value)}}
-            />
-            <label htmlFor="expense-edit" style={{color:"red"}}>Expense</label>
-            <input type="radio" id="income-edit" value="income" 
-                checked = {transactionType === "income"}
-                onChange={(e) => {settransactionType(e.target.value)}}
-            />
-            <label htmlFor="income-edit" style={{color:"green"}}>Income</label>
-            <div>
+            <div className="add-transaction mg-btm-0p5rem">
+                <input type="text" placeholder="Description" required 
+                    value={description} 
+                    onChange={(e) => {setDescriptionEdit(e.target.value)}}
+                />
+                <input type="number" placeholder="Amount" required 
+                    value={transactionAmount}
+                    onChange={(e) => {setTransactionAmount(e.target.value)}}
+                />
+                <input type="radio" id="expense-edit" value="expense"
+                    checked = {transactionType === "expense"}
+                    onChange={(e) => {settransactionType(e.target.value)}}
+                />
+                <label htmlFor="expense-edit" style={{color:"red"}}>Expense</label>
+                <input type="radio" id="income-edit" value="income" 
+                    checked = {transactionType === "income"}
+                    onChange={(e) => {settransactionType(e.target.value)}}
+                />
+                <label htmlFor="income-edit" style={{color:"green"}}>Income</label>
+            </div>
+            <div className="mg-btm-0p5rem">
                 <button type="submit" className="btn btn-primary btn-sm me-2">Save</button>
                 <button type="button" className="btn btn-secondary btn-sm" onClick={onCancel}>Cancel</button>
             </div>
